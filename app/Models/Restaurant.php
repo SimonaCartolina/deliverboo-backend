9a5dfe981b179cc8a3943da6aa69398d394e688b
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-09-14 18:33:11 +0200

    created relation with plate

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\Plate as ModelsPlate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,8 @@ class Restaurant extends Model
     use HasFactory;
     protected $table = 'restaurants';
     protected $fillable = ['name', 'image', 'address', 'opening_time', 'P_IVA'];
+
+    public function plate(){
+        return $this->hasMany(Plate::class);
+    }
 }
