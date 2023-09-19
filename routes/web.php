8a7e7d837665f79d1cb34ae990dc4dfb1b53a451
Simona Cartolina <simonacartolina@hotmail.com>
2023-09-19 16:39:52 +0200

    added auth route for the admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController as HomeController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RestaurantController as RestaurantController;
+use Illuminate\Routing\Controllers\Middleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +16,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes();
+
+
+// Route::get('guest/index', [RestaurantController::class, 'index'])->name('index');
+// Route::get('guest/index/guest/menu', [RestaurantController::class, 'show'])->name('guest.menu');
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+});
