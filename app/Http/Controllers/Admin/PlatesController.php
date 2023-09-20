a38d3808c8c854816901f41a0d20b43e1f1d927f
LucaMazzola02 <lucamazzola2002@gmail.com>
2023-09-20 12:28:19 +0200

    plates create & edit crud

---
+++ app/Http/Controllers/Admin/PlatesController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+use App\Models\Plate;
+
+
+class PlatesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $platesList = Plate::all();
+
+        return view('guest.menu', compact('platesList'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        $platesList= Plate::all();
+
+        return view('guest.create', compact('platesList'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+        $data = $request->all();
+
+        if ($request->hasFile('image')){
+            $img_path = Storage::put('uploads', $request['image']);
+            $data['image'] = $img_path;
+        }
+        $newPlate = new Plate();
+        $newPlate->fill($data);
+        $newPlate->save();
+        return redirect()->route('guest.menu')->with('created', $newPlate->id);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- resources/views/guest/create.blade.php
+++ resources/views/guest/create.blade.php
@@ -9,14 +9,11 @@
     <div class="row justify-content-around">
         <div class="col-8 my-5">
             <h1 class="fw-bolder" style="color:black">
-                Make your restaurant!
+                Make your plate!
             </h1>
         </div>
-        <form class="col-8 bg-light p-3 rounded bg-white " action="{{ route('admin.store') }}" method="POST" enctype="multipart/form-data">
+        <form class="col-8 bg-light p-3 rounded bg-white " action="{{ route('guest.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
-
-            <div class="mb-3">
-
 
                 @error('image')
                     <div class="alert alert-danger">{{ $message }}</div>
@@ -29,8 +26,6 @@
                     <input type="file" name="image" id="image" class="form-control" placeholder="Upload your image" value="{{ old('image', '') }}">
                 </div>
 
-            </div>
-
             <div class="mb-3">
 
                 <label for="name" class="form-label">
@@ -40,31 +35,24 @@
             </div>
 
             <div class="mb-3">
-                <label for="opening_time" class="form-label">
-                    Opening_time
+                <label for="description" class="form-label">
+                    Description:
                 </label>
-                <input type="text" class="form-control" id="opening_time" name="opening_time">
+                <input type="text" class="form-control" id="description" name="description">
             </div>
 
             <div class="mb-3">
-                <label for="address" class="form-label">
-                    Address:
+                <label for="price" class="form-label">
+                    Price:
                 </label>
-                <input type="text" class="form-control" id="address" name="address">
-            </div>
-
-            <div class="mb-3">
-                <label for="P_IVA" class="form-label">
-                    P_IVA:
-                </label>
-                <input type="text" class="form-control" id="P_IVA" name="P_IVA">
+                <input type="text" class="form-control" id="price" name="price">
             </div>
 
             <button type="submit" class="btn btn-success">
                 Create
             </button>
 
-            <a href="{{ route('admin.index')}}" class="btn btn-danger">
+            <a href="{{ route('guest.menu')}}" class="btn btn-danger">
                 Delete
             </a>
         </form>
