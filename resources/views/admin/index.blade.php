83abb5b76c96bf1211996a86d7147e3a875c981f
Simona Cartolina <simonacartolina@hotmail.com>
2023-09-27 02:06:38 +0200

    modified button

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -55,14 +55,14 @@
 
                     <a href="{{ route('admin.menu.menu')}}" class="btn btn-sm btn-success">
 
-                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512" style="fill: #FFFFFF">
+                        <svg xmlns="http://www.w3.org/2000/svg" height="1.2rem" viewBox="0 0 512 512" style="fill: #FFFFFF">
                             <path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z" />
                         </svg>
                     </a>
 
 
-                    <a class="btn btn-sm btn-success me-2 my-4" href="{{ route('admin.edit', $restaurant->id) }}">
-                        <svg xmlns="http://www.w3.org/2000/svg" style="fill: #ffffff" height="1em" viewBox="0 0 512 512">
+                    <a class="btn-sm me-2 my-4 btn btn-warning" href="{{ route('admin.edit', $restaurant->id) }}">
+                        <svg xmlns="http://www.w3.org/2000/svg" style="fill: #ffffff" height="1.2rem" viewBox="0 0 512 512">
                             <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
                         </svg>
                     </a>
