41748ca73737f36009d4b458fc7fb1e4b9f32d8e
Simona Cartolina <simonacartolina@hotmail.com>
2023-11-01 19:32:57 +0100

    updated menu

--- resources/views/admin/menu/menu.blade.php
+++ resources/views/admin/menu/menu.blade.php
@@ -8,10 +8,10 @@
 
 
 <div class="d-flex flex-wrap" style="height:100vh;">
-    <div class=" ps-4" style="position:relative; left:100%;">
+    <div class="ps-4" style="position:relative; left:100%;">
         <div>
             <h1 class="fw-bolder" style="color:#00BAAB">Menù:</h1>
-            <button class="button-container btn mt-2 p-2" style="background-color:#00BAAB">
+            <button class="button-container btn mt-2" style="background-color:#00BAAB">
                 <a href="{{ route('admin.menu.create')}}">
 
                     <svg xmlns="http://www.w3.org/2000/svg" height="2rem" viewBox="0 0 448 512">
@@ -27,20 +27,20 @@
 
         @if ($plates->count() > 0)
         @foreach($plates as $plate)
-        <div class="plate-card my-4 mx-4 d-flex">
+        <div class="plate-card my-4 mx-4">
             <ul>
                 @if (str_starts_with($plate->image, 'http'))
                 <img src="{{ $plate->image }}" alt="{{ $plate->name }}">
                 @else
                 <img src="{{ asset('storage/' . $plate->image) }}" alt="{{ $plate->name }}">
                 @endif
-                <li class="my-4">
-                    <h2>{{ $plate->name }}</h2>
-                    <p>{{ $plate->description }}</p>
-                    <p>Prezzo: {{ $plate->price }} €</p>
+                <li>
+                    <h3>{{ $plate->name }}</h4>
+                        <p>{{ $plate->description }}</p>
+                        <p>Prezzo: {{ $plate->price }} €</p>
                 </li>
                 <li>
-                    <a class="btn btn-sm btn-success" style="width:3rem;" href="{{ route('admin.menu.edit', $plate->id) }}">
+                    <a class="btn btn-sm" style="width:2rem;" href="{{ route('admin.menu.edit', $plate->id) }}">
 
                         <svg xmlns="http://www.w3.org/2000/svg" style="fill: #ffffff" height="1em" viewBox="0 0 512 512">
                             <path d="M362.7 19.3L314.3 67.7 444.3 197.7l48.4-48.4c25-25 25-65.5 0-90.5L453.3 19.3c-25-25-65.5-25-90.5 0zm-71 71L58.6 323.5c-10.4 10.4-18 23.3-22.2 37.4L1 481.2C-1.5 489.7 .8 498.8 7 505s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L421.7 220.3 291.7 90.3z" />
@@ -75,8 +75,14 @@
         box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
     }
 
+    ul {
+        padding-left: 0;
+
+    }
+
     img {
-        width: 15rem;
+        width: 100%;
+        display: block;
     }
 
     li {
